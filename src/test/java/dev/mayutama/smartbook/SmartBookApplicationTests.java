44ac4e55ea1a8ff7872aa16d98ad6d18a9deb9c4
YudhaAP <muhyudhapratama55@gmail.com>
2025-06-21 18:15:36 +0700

    INIT PROJECT SMARTBOOK

---
+++ src/test/java/dev/mayutama/smartbook/SmartBookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mayutama.smartbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
